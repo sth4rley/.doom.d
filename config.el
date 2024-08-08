2f9a341b9ccd3a286345379d61c5e2d575de043e
Stharley <sstharley@gmail.com>
2024-08-08 10:14:40 -0300

    08/08

--- config.el
+++ config.el
@@ -1,40 +1,34 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-symbol-font' -- for symbols
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-(setq doom-font (font-spec :family "CaskaydiaMono Nerd Font Mono" :size 15)
-      doom-variable-pitch-font (font-spec :family "CaskaydiaMono Nerd Font Propo" :size 15)) ;; SPC h r f -> reload font
-;; (setq doom-font (font-spec :family "CaskaydiaMono Nerd Font Mono" :size 15 :weight 'SemiBold)) ;; SPC h r f -> reload font
+(setq doom-theme 'doom-1337
+      doom-font (font-spec :family "JetBrainsMono Nerd Font Mono" :size 15)
+      doom-variable-pitch-font (font-spec :family "DejaVu Sans" :size 20))
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-material-dark)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type `relative)
+;; Line numbers are pretty slow all around. The performance boost of disabling
+;; them outweighs the utility of always keeping them on.
+(setq display-line-numbers-type nil)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
+
+(setq fancy-splash-image (file-name-concat doom-user-dir "cacochan.png"))
+
+;; Hide the menu for as minimalistic a startup screen as possible.
+;; (setq +doom-dashboard-functions '(doom-dashboard-widget-banner))
+
+;; scratch buffer org mode
+(setq doom-scratch-initial-major-mode 'org-mode)
+
+;;; :editor evil
+;; Focus new window after splitting
+(setq evil-split-window-below t
+      evil-vsplit-window-right t)
+
+;; Prevents some cases of Emacs flickering.
+(add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
+
+(fset 'rainbow-delimiters-mode #'ignore)
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
@@ -60,14 +54,10 @@
 ;;   `require' or `use-package'.
 ;; - `map!' for binding new keys
 ;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+;; To get information about any of these functions/macros press 'K' This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces, etc).
+;; You can also try 'gd' to jump to their definition and see how they are implemented.
+
 
 (after! doom-modeline
   (setq doom-modeline-check-simple-format t)
@@ -75,22 +65,6 @@
   ;; An evil mode indicator is redundant with cursor shape
   (setq doom-modeline-modal nil)
   )
-
-
-(fset 'rainbow-delimiters-mode #'ignore)
-
-(setq fancy-splash-image (file-name-concat doom-user-dir "cacochan.png"))
-;; Hide the menu for as minimalistic a startup screen as possible.
-;; (setq +doom-dashboard-functions '(doom-dashboard-widget-banner))
-
-;; scratch buffer org mode
-(setq doom-scratch-initial-major-mode 'org-mode)
-
-
-;;; :editor evil
-;; Focus new window after splitting
-(setq evil-split-window-below t
-      evil-vsplit-window-right t)
 
 (after! lsp-mode
   (setq lsp-enable-symbol-highlighting nil
@@ -106,7 +80,6 @@
         lsp-ui-sideline-enable nil  ; no more useful than flycheck
         )
   )
-
 
 ;; packages
 (use-package! devdocs
